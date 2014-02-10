b4c26ded22c6abdcb4f1656ff0bc166051fb49a1
suisho <suisho000000@gmail.com>
2014-02-10 21:49:22 +0900

    fix maclink

--- maclink.sh
+++ maclink.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-SUBLIME="~/Library/Application Support/Sublime Text 3"
+SUBLIME=~/Library/Application\ Support/Sublime\ Text\ 3
 PWD=`pwd`
 #echo ls ${SUBLIME}
 
-ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
-ln -s "/Applications/Sublime Text2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime2
+ln -s -f "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime
+ln -s -f "/Applications/Sublime Text2.app/Contents/SharedSupport/bin/subl" /usr/local/bin/sublime2
 
 ln -s -f "${PWD}/Data/Packages" "${SUBLIME}"
 ln -s -f "${PWD}/Data/Installed Packages" "${SUBLIME}"
